2c0a5e74081dfcf4e7c9eba61a41988cbd475c02
Nobunaga20 <rfridge73@gmail.com>
2025-03-08 21:28:09 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,19 +10,5 @@
                 <a href="{{ route('admin.items.index') }}" class="btn btn-light">Go to Items</a>
             </div>
         </div>
-        <div class="col-md-4">
-            <div class="card text-white bg-success p-3">
-                <h4>Manage Categories</h4>
-                <p>Organize and edit item categories.</p>
-                <a href="{{ route('admin.categories.index') }}" class="btn btn-light">Go to Categories</a>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card text-white bg-warning p-3">
-                <h4>View Invoices</h4>
-                <p>Review customer purchase history.</p>
-                <a href="{{ route('admin.invoice.index') }}" class="btn btn-light">Go to Invoices</a>
-            </div>
-        </div>
     </div>
 @endsection
